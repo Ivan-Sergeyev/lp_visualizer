cafe60c33a18575f2b43c4bb536f0402ee760a7f
Ivan Sergeev <i.i.sergeyev@gmail.com>
2026-04-12 23:32:17 +0200

    Added functionality for adding and removing constraints in UI. Added latex rendering for variables and signs.

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import dash
-from dash import ALL, dcc, html, callback, Input
-
+import dash_iconify
+import dash_latex as dl
 import plotly.graph_objects as go
 
-from callbacks.model_state import ConstraintDict, ModelState, ObjectiveDict, constraint_id_to_name
+from callbacks.model_state import ConstraintDict, ModelState, ObjectiveDict
 
 
 initial_objective = ObjectiveDict(x_coeff=6.0, y_coeff=9.0, sense='max')
 
 initial_constraints = [
-    ConstraintDict(id=0, name=constraint_id_to_name(0), x_coeff=2.0, y_coeff=3.0, sense='<=', rhs=12.0),
-    ConstraintDict(id=1, name=constraint_id_to_name(1), x_coeff=1.0, y_coeff=1.0, sense='<=', rhs=5.0),
-    ConstraintDict(id=2, name=constraint_id_to_name(2), x_coeff=1.0, y_coeff=0.0, sense='>=', rhs=0.0),
-    ConstraintDict(id=3, name=constraint_id_to_name(3), x_coeff=0.0, y_coeff=1.0, sense='>=', rhs=0.0),
+    ConstraintDict(name='0', x_coeff=2.0, y_coeff=3.0, sense='<=', rhs=12.0),
+    ConstraintDict(name='1', x_coeff=1.0, y_coeff=1.0, sense='<=', rhs=5.0),
+    ConstraintDict(name='2', x_coeff=1.0, y_coeff=0.0, sense='>=', rhs=0.0),
+    ConstraintDict(name='3', x_coeff=0.0, y_coeff=1.0, sense='>=', rhs=0.0),
 ]
 
 model_state = ModelState(initial_objective, initial_constraints)
 
-objective_row_component = html.Div(
+objective_row_component = dash.html.Div(
     id='objective-row',
     children=[
-        dcc.Dropdown(
+        dash.dcc.Dropdown(
             id='objective-sense',
             options=['max', 'min'],
             value=initial_objective.sense,
@@ -28,7 +28,7 @@ objective_row_component = html.Div(
             searchable=False,
             placeholder='Objective sense',
         ),
-        dcc.Input(
+        dash.dcc.Input(
             id='objective-x-coeff',
             className='coeff-input',
             type='number',
@@ -37,16 +37,16 @@ objective_row_component = html.Div(
             debounce=True,
             placeholder='Coefficient for x',
         ),
-        html.Div(
-            'x',
+        dash.html.Div(
+            dl.DashLatex(r'$x$'),
             className='variable-label',
         ),
-        html.Div(
-            '+',
+        dash.html.Div(
+            dl.DashLatex(r'$+$'),
             id='objective-sign-label',
             className='sign-label',
         ),
-        dcc.Input(
+        dash.dcc.Input(
             id='objective-y-coeff',
             className='coeff-input',
             type='number',
@@ -55,20 +55,20 @@ objective_row_component = html.Div(
             debounce=True,
             placeholder='Coefficient for y',
         ),
-        html.Div(
-            'y',
+        dash.html.Div(
+            dl.DashLatex(r'$y$'),
             className='variable-label'
         ),
     ],
 )
 
-def constraint_component_builder(constraint: ConstraintDict) -> html.Div:
-    return html.Div(
-        id={'type': 'constraint-row', 'index': constraint.id},
+def constraint_component_builder(constraint: ConstraintDict) -> dash.html.Div:
+    return dash.html.Div(
+        id={'type': 'constraint-row', 'name': constraint.name},
         className='constraint-row',
         children=[
-            dcc.Input(
-                id={'type': 'constraint-x-coeff', 'index': constraint.id},
+            dash.dcc.Input(
+                id={'type': 'constraint-x-coeff', 'name': constraint.name},
                 className='coeff-input',
                 type='number',
                 inputMode='numeric',
@@ -76,17 +76,17 @@ def constraint_component_builder(constraint: ConstraintDict) -> html.Div:
                 debounce=True,
                 placeholder='Coefficient for x',
             ),
-            html.Div(
-                'x',
+            dash.html.Div(
+                dl.DashLatex(r'$x$'),
                 className='variable-label',
             ),
-            html.Div(
-                '+',
-                id={'type': 'constraint-sign-label', 'index': constraint.id},
+            dash.html.Div(
+                dl.DashLatex(r'$+$'),
+                id={'type': 'constraint-sign-label', 'name': constraint.name},
                 className='sign-label',
             ),
-            dcc.Input(
-                id={'type': 'constraint-y-coeff', 'index': constraint.id},
+            dash.dcc.Input(
+                id={'type': 'constraint-y-coeff', 'name': constraint.name},
                 className='coeff-input',
                 type='number',
                 inputMode='numeric',
@@ -94,64 +94,97 @@ def constraint_component_builder(constraint: ConstraintDict) -> html.Div:
                 debounce=True,
                 placeholder='Coefficient for y',
             ),
-            html.Div(
-                'y',
+            dash.html.Div(
+                dl.DashLatex(r'$y$'),
                 className='variable-label',
             ),
-            dcc.Dropdown(
-                id={'type': 'constraint-sense', 'index': constraint.id},
+            dash.dcc.Dropdown(
+                id={'type': 'constraint-sense', 'name': constraint.name},
                 className='constraint-sense',
-                options=['<=', '>=', '='],
+                options={
+                    '<=' : '≤',
+                    '>=' : '≥',
+                    '=' : '=',
+                },
                 value=constraint.sense,
                 clearable=False,
                 searchable=False,
                 placeholder='Constraint sense',
             ),
-            dcc.Input(
-                id={'type': 'constraint-rhs', 'index': constraint.id},
+            dash.dcc.Input(
+                id={'type': 'constraint-rhs', 'name': constraint.name},
                 className='coeff-input',
                 type='number',
                 inputMode='numeric',
                 value=constraint.rhs,
                 debounce=True,
                 placeholder='Right hand side',
-            )
+            ),
+            dash.dcc.Button(
+                id={'type': 'remove-constraint-button', 'name': constraint.name},
+                name=constraint.name,
+                className='remove-constraint-button',
+                title='Remove constraint',
+                children=[
+                    dash_iconify.DashIconify(icon='mdi:trash-can-outline')
+                ],
+                n_clicks=0,
+            ),
         ],
     )
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(
+# note: there's an alternative way to render math in dash by using mathjax.
+# it's done by changing app initialization to:
+# ```python
+# app = dash.Dash(__name__, external_scripts=['https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML'] # unfortunately, external)
+# ```
+# then formulas are rendered by writing, for example, '\\(x\\)' or r'\(x\)' instead of dl.DashLatex(r'$x$')
+# however, the mathjax approach is unfortunately unstable---math rendering breaks after a few page reloads
+
+app.layout = dash.html.Div(
     children=[
-        html.Div(
+        dash.html.Div(
             id='model-container',
             children=[
                 objective_row_component,
-                html.Div(
+                dash.html.Div(
                     id='constraints-container',
                     children=[
-                        html.Div(
+                        dash.html.Div(
                             's.t.',
                             className='st-label',
                         ),
-                        html.Div(
+                        dash.html.Div(
                             id='constraints-list',
                             children=[
                                 constraint_component_builder(constraint) for constraint in initial_constraints
+                            ] + [
+                                dash.html.Button(
+                                    id='add-constraint-button',
+                                    className='add-constraint-button',
+                                    title='Add constraint',
+                                    children=[
+                                        'Add constraint',
+                                        # dash_iconify.DashIconify(icon='mdi:plus')
+                                    ],
+                                    n_clicks=len(initial_constraints) - 1,
+                                ),
                             ],
                         ),
-                    ]
-                )
+                    ],
+                ),
             ],
         ),
-        html.Div(
+        dash.html.Div(
             id='visualization-container',
             children=[
-                dcc.Graph(
+                dash.dcc.Graph(
                     id='graph',
                     figure=go.Figure(),
                 ),
-                html.Div(
+                dash.html.Div(
                     'Optimal value: ',
                     id='solution-value',
                 )
@@ -160,69 +193,160 @@ app.layout = html.Div(
     ],
 )
 
-@callback(
-    Input('objective-sense', 'value')
+
+def prase_trigger(trigger: dict[str, str]) -> tuple[str, str]:
+    return trigger['prop_id'].split(':')[1].split(',')[0].strip("\""), trigger['value']
+
+
+@dash.callback(
+    dash.Input('objective-sense', 'value'),
+    prevent_initial_call=True,
 )
 def update_objective_sense(selected_sense):
-    print(f'Received objective sense change to {selected_sense} in callback.')
+    print(f' --- In update_objective_sense({selected_sense})')
     if selected_sense:
+        print(f'Updating model sense with {selected_sense}')
         return model_state.update_objective_sense(selected_sense)
 
-@callback(
-    Input('objective-x-coeff', 'value'),
-    Input('objective-y-coeff', 'value')
+
+@dash.callback(
+    dash.Input('objective-x-coeff', 'value'),
+    dash.Input('objective-y-coeff', 'value'),
+    prevent_initial_call=True,
 )
 def update_objective(x_coeff, y_coeff):
-    print(f'Received objective coefficient change to x: {x_coeff}, y: {y_coeff} in callback.')
+    print(f' --- In update_objective({x_coeff}, {y_coeff})')
     if x_coeff is not None and y_coeff is not None:
         return model_state.update_objective(x_coeff, y_coeff)
 
-@callback(
-    # Output({'type': 'constraint-line', 'index': MATCH}, 'value'),
-    Input({'type': 'constraint-x-coeff', 'index': ALL}, 'value'),
-    # State({'type': 'constraint-x-coeff', 'index': MATCH}, 'id'),
+
+@dash.callback(
+    dash.Input({'type': 'constraint-x-coeff', 'name': dash.ALL}, 'value'),
+    prevent_initial_call=True,
 )
 def update_constraint_x_coefficient(_x_coeffs):
+    print(f' --- In update_constraint_x_coefficient({_x_coeffs})')
     for trigger in dash.callback_context.triggered:
-        constraint_id = int(trigger['prop_id'].split(':')[1].split(',')[0])
-        x_coeff = float(trigger['value'])
-        print(f'Triggered by {constraint_id} with value {x_coeff}')
-        return model_state.update_constraint_x_coeff(constraint_id_to_name(constraint_id), x_coeff)
+        constraint_name, x_coeff = prase_trigger(trigger)
+        print(f'Triggered by {constraint_name} with value {x_coeff}')
+        return model_state.update_constraint_x_coeff(constraint_name, float(x_coeff))
 
-@callback(
-    Input({'type': 'constraint-y-coeff', 'index': ALL}, 'value'),
+
+@dash.callback(
+    dash.Input({'type': 'constraint-y-coeff', 'name': dash.ALL}, 'value'),
+    prevent_initial_call=True,
 )
 def update_constraint_y_coefficient(_y_coeffs):
+    print(f' --- In update_constraint_y_coefficient ({_y_coeffs})')
     for trigger in dash.callback_context.triggered:
-        constraint_id = int(trigger['prop_id'].split(':')[1].split(',')[0])
-        y_coeff = float(trigger['value'])
-        print(f'Triggered by {constraint_id} with value {y_coeff}')
-        return model_state.update_constraint_y_coeff(constraint_id_to_name(constraint_id), y_coeff)
+        constraint_name, y_coeff = prase_trigger(trigger)
+        print(f'Triggered by {constraint_name} with value {y_coeff}')
+        return model_state.update_constraint_y_coeff(constraint_name, float(y_coeff))
 
-@callback(
-    Input({'type': 'constraint-sense', 'index': ALL}, 'value'),
+
+@dash.callback(
+    dash.Input({'type': 'constraint-sense', 'name': dash.ALL}, 'value'),
+    prevent_initial_call=True,
 )
 def update_constraint_sense(_senses):
+    print(f' --- In update_constraint_sense({_senses})')
     for trigger in dash.callback_context.triggered:
-        constraint_id = int(trigger['prop_id'].split(':')[1].split(',')[0])
-        sense = trigger['value']
-        print(f'Triggered by {constraint_id} with value {sense}')
-        return model_state.update_constraint_sense(constraint_id_to_name(constraint_id), sense)
+        constraint_name, sense = prase_trigger(trigger)
+        print(f'Triggered by {constraint_name} with value {sense}')
 
-@callback(
-    Input({'type': 'constraint-rhs', 'index': ALL}, 'value'),
+        if sense is None:
+            continue
+
+        return model_state.update_constraint_sense(constraint_name, sense)
+
+
+@dash.callback(
+    dash.Input({'type': 'constraint-rhs', 'name': dash.ALL}, 'value'),
+    prevent_initial_call=True,
 )
 def update_constraint_rhs(_rhs_values):
+    print(f' --- In update_constraint_rhs({_rhs_values})')
     for trigger in dash.callback_context.triggered:
-        constraint_id = int(trigger['prop_id'].split(':')[1].split(',')[0])
-        rhs_value = float(trigger['value'])
-        print(f'Triggered by {constraint_id} with value {rhs_value}')
-        return model_state.update_constraint_rhs(constraint_id_to_name(constraint_id), rhs_value)
+        constraint_name, rhs_value = prase_trigger(trigger)
+        print(f'Triggered by {constraint_name} with value {rhs_value}')
+
+        if rhs_value is None:
+            continue
+
+        return model_state.update_constraint_rhs(constraint_name, float(rhs_value))
+
+
+def _add_constraint(add_n_clicks: int) -> dash.Patch:
+    new_constraint = ConstraintDict(
+        name=str(add_n_clicks),
+        x_coeff=0.0,
+        y_coeff=0.0,
+        sense='<=',
+        rhs=0.0,
+    )
+
+    model_state.add_constraint(new_constraint)
+    # todo: propagate graph update
+
+    patch = dash.Patch()
+    patch.insert(-1, constraint_component_builder(new_constraint))
+    return patch
+
+
+def _remove_constraint(constraints_container: list, constraint_name: str) -> dash.Patch:
+    patch = dash.Patch()
+    constraint_found = False
+
+    for pos, elem in enumerate(constraints_container[1]['props']['children']):
+        elem_id = elem['props']['id']
+        if not isinstance(elem_id, dict):
+            continue
+
+        if elem_id['type'] != 'constraint-row' or elem_id['name'] != constraint_name:
+            continue
+
+        constraint_found = True
+        model_state.remove_constraint(constraint_name)
+        # todo: propagate graph update
+        del patch[pos]
+
+    if not constraint_found:
+        raise ValueError(f'Constraint with name {constraint_name} not found in constraints container list')
+
+    return patch
+
+
+@dash.callback(
+    dash.Output('constraints-list', 'children', True),
+    dash.Input('add-constraint-button', 'n_clicks'),
+    dash.Input({'type': 'remove-constraint-button', 'name': dash.ALL}, 'n_clicks'),
+    dash.State('constraints-container', 'children'),
+    prevent_initial_call=True,
+    running=[
+        (dash.Output('add-constraint-button', 'disabled'), True, False),
+        (dash.Output({'type': 'remove-constraint-button', 'name': dash.ALL}, 'disabled'), True, False),
+    ],
+)
+def add_or_remove_constraint(add_n_clicks: int, _remove_ns_clicks: list[int], constraints_container: list):
+    print(f' --- In add_constraint({add_n_clicks})')
+
+    for trigger in dash.callback_context.triggered:
+        trigger_prop_id = dash.callback_context.triggered_prop_ids[trigger['prop_id']]
+        value = trigger['value']
+        print(f'trigger_prop_id: {trigger_prop_id}, value: {value}')
+
+        if trigger_prop_id == 'add-constraint-button':
+            return _add_constraint(add_n_clicks)
+
+        if trigger_prop_id['type'] == 'remove-constraint-button':
+            constraint_name = trigger_prop_id['name']
+            if value is None or value == 0:
+                continue
+            return _remove_constraint(constraints_container, constraint_name)
+
 
 # todo: add graph drawing callbacks
 
-# todo: add functionality for adding new constraint (button, callback)
-# todo: add functionality for removing existing constraint (button, callback)
 # todo: add functionality for toggling constraints on/off (buttons, callbacks, greying out)
 
 # note for later: if we want to horizontally align bottom edge of 's.t.' label with bottom edge of first constraint row,
@@ -230,7 +354,7 @@ def update_constraint_rhs(_rhs_values):
 # for dynamically adding/removing constraint rows while keeping 's.t.' label properly positioned.
 
 # note for later: how to disable a button while a callback is running
-# running=[(Output('submit-button', 'disabled'), True, False)]
+# running=[(dash.Output('submit-button', 'disabled'), True, False)]
 
 # note for later: list of useful links
 # very important for implementing callbacks!!! https://dash.plotly.com/pattern-matching-callbacks
@@ -240,7 +364,15 @@ def update_constraint_rhs(_rhs_values):
 # https://dash.plotly.com/flexible-callback-signatures
 # https://dash.plotly.com/callback-gotchas
 
-# note: python-mip documentation https://docs.python-mip.com/en/latest/index.html
+# note: python-mip documentation https://docs.python-mip.com/en/latest/name.dash.html
+
+# note: can make it look pretty
+# - add nicer components with Dash Mantine Components (DMC) https://www.dash-mantine-components.com/
+# - add icons with dash-iconify https://pypi.org/project/dash-iconify/ https://github.com/snehilvj/dash-iconify
+
+# known issues:
+# - reload browser page => UI refreshes, but not LP model => stale constraint IDs in UI => errors when changing constraints
+# - with debug=True, model is optimized twice at startup
 
 if __name__ == '__main__':
     app.run(debug=True)
--- callbacks/model_state.py
+++ callbacks/model_state.py
@@ -1,25 +1,8 @@
 from dataclasses import dataclass
+import traceback
 from typing import Tuple
 
 import mip
-
-
-@dataclass
-class ConstraintDict:
-    id: int
-    name: str
-    x_coeff: float
-    y_coeff: float
-    sense: str
-    rhs: float
-
-
-def constraint_id_to_name(constraint_id: int) -> str:
-    return f'constraint_{constraint_id}'
-
-
-def constraint_name_to_id(constraint_name: str) -> int:
-    return int(constraint_name.split('_')[1])
 
 
 @dataclass
@@ -39,16 +22,38 @@ def objective_sense_to_mip(sense: str) -> str:
             raise ValueError(f'Unrecognized objective sense {sense}.')
 
 
-def constraint_sense_to_mip(sense: str) -> str:
-    match sense:
-        case '<' | '<=' | '≤' | mip.LESS_OR_EQUAL:
-            return mip.LESS_OR_EQUAL
-        case '>' | '>=' | '≥' | mip.GREATER_OR_EQUAL:
-            return mip.GREATER_OR_EQUAL
-        case '=' | '==' | mip.EQUAL:
-            return mip.EQUAL
-        case _:
-            raise ValueError(f'Unrecognized constraintaint sense {sense}.')
+@dataclass
+class ConstraintDict:
+    name: str
+    x_coeff: float
+    y_coeff: float
+    sense: str
+    rhs: float
+
+    @property
+    def sense_mip(self) -> str:
+        match self.sense:
+            case '<' | '<=' | '≤' | mip.LESS_OR_EQUAL:
+                return mip.LESS_OR_EQUAL
+            case '>' | '>=' | '≥' | mip.GREATER_OR_EQUAL:
+                return mip.GREATER_OR_EQUAL
+            case '=' | '==' | mip.EQUAL:
+                return mip.EQUAL
+            case _:
+                raise ValueError(f'Unrecognized constraint sense {self.sense}.')
+
+    def convert_sense_to_mip(self):
+        self.sense = self.sense_mip()
+
+
+def constraint_dict_from_mip(constraint: mip.Constr, x: mip.Var, y: mip.Var) -> ConstraintDict:
+    return ConstraintDict(
+        name=get_mip_constraint_name(constraint),
+        x_coeff=get_mip_constraint_coeff(constraint, x),
+        y_coeff=get_mip_constraint_coeff(constraint, y),
+        sense=get_mip_constraint_sense(constraint),
+        rhs=get_mip_constraint_rhs(constraint)
+    )
 
 
 def get_mip_constraint_name(constraint: mip.Constr) -> str:
@@ -56,7 +61,7 @@ def get_mip_constraint_name(constraint: mip.Constr) -> str:
 
 
 def get_mip_constraint_coeff(constraint: mip.Constr, variable: mip.Var) -> float:
-    return constraint.expr.expr.get(variable, 0)
+    return constraint.expr.expr.get(variable, 0.)
 
 
 def get_mip_constraint_sense(constraint: mip.Constr) -> str:
@@ -89,31 +94,20 @@ class ModelState:
         self.x = self.model.add_var(name='x', lb=-mip.INF, ub=mip.INF, var_type=mip.CONTINUOUS)
         self.y = self.model.add_var(name='y', lb=-mip.INF, ub=mip.INF, var_type=mip.CONTINUOUS)
 
-        self._update_objective(objective.x_coeff, objective.y_coeff)
+        self._update_objective_coeffs(objective.x_coeff, objective.y_coeff)
         self._update_sense(objective.sense)
 
         for constraint in constraints:
-            self._add_constraint(constraint.name, constraint.x_coeff, constraint.y_coeff, constraint.sense, constraint.rhs)
+            self._add_constraint(constraint)
 
         self._optimize()
         # todo: return graph callback result to update entire graph
 
-    def _save_to_file(self, filename: str):
-        self.model.write(filename)
-
-    def _load_from_file(self, filename: str):
-        model = mip.Model()
-        model.read(filename)
-
-        if len(model.vars) != 2:
-            raise ValueError('Model must have exactly 2 variables.')
-
-        if model.num_int > 0:
-            raise ValueError('Model must not have integer variables.')
-
-        self.model = model
-
     def _optimize(self):
+        print(f'Called _optimize()')
+        for line in traceback.format_stack():
+            print(line.strip())
+
         status = self.model.optimize()
         self.__debug()
 
@@ -142,44 +136,56 @@ class ModelState:
         for constraint in self.model.constrs:
             print(constraint)
 
-    def _update_objective(self, x_coeff: float, y_coeff: float):
+    def _update_objective_coeffs(self, x_coeff: float, y_coeff: float):
         self.model.objective = mip.LinExpr(variables=[self.x, self.y], coeffs=[x_coeff, y_coeff])
 
     def _update_sense(self, sense: str):
         self.model.sense = objective_sense_to_mip(sense)
 
     def _get_constraint(self, name: str) -> mip.Constr:
+        print(f'looking for constr {name}')
+        for constr in self.model.constrs:
+            print(constr.name)
+
         constraint = self.model.constr_by_name(name)
 
         if constraint is None:
-            raise ValueError(f'constraintaint with name {name} not found.')
+            raise ValueError(f'Constraint with name {name} not found.')
 
         return constraint
 
-    def _add_constraint(self, name: str, x_coeff: float, y_coeff: float, sense: str, rhs: float):
-        constraint = mip.LinExpr(variables=[self.x, self.y], coeffs=[x_coeff, y_coeff], sense=constraint_sense_to_mip(sense), const=-rhs)
-        self.model.add_constr(constraint, name=name)
+    def _add_constraint(self, constraint: ConstraintDict):
+        lin_expr = mip.LinExpr(
+            variables=[self.x, self.y],
+            coeffs=[constraint.x_coeff, constraint.y_coeff],
+            sense=constraint.sense_mip,
+            const=-constraint.rhs,
+        )
+        self.model.add_constr(lin_expr, name=constraint.name)
 
     def _remove_constraint(self, constraint: mip.Constr):
         self.model.remove(constraint)
 
     def _update_constraint_x_coeff(self, constraint: mip.Constr, x_coeff: float):
         # note: constraint coeffs cannot be updated in-place in mip, so remove and re-add constraint with new coeffs
-        name, _, y_coeff, sense, rhs = deconstruct_mip_constraint(constraint, self.x, self.y)
+        updated_constraint = constraint_dict_from_mip(constraint, self.x, self.y)
+        updated_constraint.x_coeff = x_coeff
         self._remove_constraint(constraint)
-        self._add_constraint(name, x_coeff, y_coeff, sense, rhs)
+        self._add_constraint(updated_constraint)
 
     def _update_constraint_y_coeff(self, constraint: mip.Constr, y_coeff: float):
         # note: constraint coeffs cannot be updated in-place in mip, so remove and re-add constraint with new coeffs
-        name, x_coeff, _, sense, rhs = deconstruct_mip_constraint(constraint, self.x, self.y)
+        updated_constraint = constraint_dict_from_mip(constraint, self.x, self.y)
+        updated_constraint.y_coeff = y_coeff
         self._remove_constraint(constraint)
-        self._add_constraint(name, x_coeff, y_coeff, sense, rhs)
+        self._add_constraint(updated_constraint)
 
     def _update_constraint_sense(self, constraint: mip.Constr, sense: str):
         # note: constraint sense cannot be updated in-place in mip, so remove and re-add constraint with new sense
-        name, x_coeff, y_coeff, _, rhs = deconstruct_mip_constraint(constraint, self.x, self.y)
+        updated_constraint = constraint_dict_from_mip(constraint, self.x, self.y)
+        updated_constraint.sense = sense
         self._remove_constraint(constraint)
-        self._add_constraint(name, x_coeff, y_coeff, sense, rhs)
+        self._add_constraint(updated_constraint)
 
     def _update_constraint_rhs(self, constraint: mip.Constr, rhs: float):
         # note: constraint right hand side can be updated in-place in mip
@@ -196,14 +202,14 @@ class ModelState:
         # todo: return graph callback result to update solution
 
     def update_objective(self, x_coeff: float, y_coeff: float):
-        self._update_objective(x_coeff, y_coeff)
+        self._update_objective_coeffs(x_coeff, y_coeff)
         self._optimize()
         # todo: return graph callback result to change objective vector direction
         # todo: return graph callback result to update solution
 
-    def add_constraint(self, name: str, x_coeff: float, y_coeff: float, sense: str, rhs: float):
+    def add_constraint(self, constraint: ConstraintDict):
         # todo: ensure user_actions sends one of '<', '>', or '=' as sense
-        self._add_constraint(name, x_coeff, y_coeff, sense, rhs)
+        self._add_constraint(constraint)
         self._optimize()
         # todo: return graph callback result to change constraint
         # todo: return graph callback result to update solution
@@ -243,4 +249,4 @@ class ModelState:
         # todo: return graph callback result to change constraint
         # todo: return graph callback result to update solution
 
-    # todo: implement constraintaint toggle logic
+    # todo: implement constraint toggle logic
